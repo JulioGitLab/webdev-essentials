dcda542e6e71b3ef158540802c143038ea162958
Julio Lab <juliolab90@hotmail.com>
2024-07-02 20:53:39 -0600

    * tic-tac-toe update:    + Canvas size and multiple figure parameters now based on constants    + Added logic for ties and to restart after finished game (win or tie)    + Added score board with current player turn    + Finished game state is now separated from checkWin

--- JS/tic-tac-toe.js
+++ JS/tic-tac-toe.js
@@ -1,6 +1,14 @@
+const cW = 375, cH = 667;
+
 function setup() {
-   let canvas = createCanvas(375, 667);
+   let canvas = createCanvas(cW, cH);
    canvas.id("tic-tac-toe-canvas");
+
+   /* // Creates a score container with a score element in paragraph
+   let scoreContainer = createDiv("Score: ");
+   scoreContainer.id("score-container");
+   let scoreSpan = createP("0");
+   scoreSpan.id("score"); */
 }
 
 const color = {
@@ -45,81 +53,63 @@ const color = {
    White:'#FFF'
 };
 
-var pTurn = 0, nCols = 3, nRows = 3, cWidth = 379, cHeight = 379, wLine = 0;
+var pTurn = 0, nCols = 3, nRows = 3, winner = -1, tie = 0; winLine = 0, restartT = 0;
 // var SYMBOLS = ["O", "X"];
 var SYMBOLS = ["🐶", "😾"];
+var score = [0, 0];
 
 var tiles = [];
 
 var checkWin = function() {
-   if (wLine === 0) {
-      if (tiles[0].label !== "" && tiles[0].label === tiles[1].label && tiles[0].label === tiles[2].label) {
-         wLine = 1;
-      } else if (tiles[3].label !== "" && tiles[3].label === tiles[4].label && tiles[3].label === tiles[5].label) {
-         wLine = 2;
-      } else if (tiles[6].label !== "" && tiles[6].label === tiles[7].label && tiles[6].label === tiles[8].label) {
-         wLine = 3;
-      } else if (tiles[0].label !== "" && tiles[0].label === tiles[3].label && tiles[0].label === tiles[6].label) {
-         wLine = 4;
-      } else if (tiles[1].label !== "" && tiles[1].label === tiles[4].label && tiles[1].label === tiles[7].label) {
-         wLine = 5;
-      } else if (tiles[2].label !== "" && tiles[2].label === tiles[5].label && tiles[2].label === tiles[8].label) {
-         wLine = 6;
-      } else if (tiles[0].label !== "" && tiles[0].label === tiles[4].label && tiles[0].label === tiles[8].label) {
-         wLine = 7;
-      } else if (tiles[6].label !== "" && tiles[6].label === tiles[4].label && tiles[6].label === tiles[2].label) {
-         wLine = 8;
-      }
+   if (tiles[0].label !== "" && tiles[0].label === tiles[1].label && tiles[0].label === tiles[2].label) {
+      (tiles[0].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 1;
+   } else if (tiles[3].label !== "" && tiles[3].label === tiles[4].label && tiles[3].label === tiles[5].label) {
+      (tiles[3].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 2;
+   } else if (tiles[6].label !== "" && tiles[6].label === tiles[7].label && tiles[6].label === tiles[8].label) {
+      (tiles[6].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 3;
+   } else if (tiles[0].label !== "" && tiles[0].label === tiles[3].label && tiles[0].label === tiles[6].label) {
+      (tiles[0].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 4;
+   } else if (tiles[1].label !== "" && tiles[1].label === tiles[4].label && tiles[1].label === tiles[7].label) {
+      (tiles[1].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 5;
+   } else if (tiles[2].label !== "" && tiles[2].label === tiles[5].label && tiles[2].label === tiles[8].label) {
+      (tiles[2].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 6;
+   } else if (tiles[0].label !== "" && tiles[0].label === tiles[4].label && tiles[0].label === tiles[8].label) {
+      (tiles[0].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 7;
+   } else if (tiles[6].label !== "" && tiles[6].label === tiles[4].label && tiles[6].label === tiles[2].label) {
+      (tiles[6].label === "🐶") ? winner = 0 : winner = 1;
+      winLine = 8;
    } else {
-      stroke(color.FireBrick);
-      strokeWeight(11);
-
-      switch (wLine) {
-         case 1:
-            line(cWidth * 0.16, cHeight * 0.09, cWidth * 0.16, cHeight * 0.91);
-            break;
-         case 2:
-            line(cWidth * 0.5, cHeight * 0.09, cWidth * 0.5, cHeight * 0.91);
-            break;
-         case 3:
-            line(cWidth * 0.84, cHeight * 0.09, cWidth * 0.84, cHeight * 0.91);
-            break;
-         case 4:
-            line(cWidth * 0.09, cHeight * 0.15, cWidth * 0.91, cHeight * 0.15);
-            break;
-         case 5:
-            line(cWidth * 0.09, cHeight * 0.5, cWidth * 0.91, cHeight * 0.5);
-            break;
-         case 6:
-            line(cWidth * 0.09, cHeight * 0.83, cWidth * 0.91, cHeight * 0.83);
-            break;
-         case 7:
-            line(cWidth * 0.13, cHeight * 0.13, cWidth * 0.87, cHeight * 0.87);
-            break;
-         case 8:
-            line(cWidth * 0.87, cHeight * 0.13, cWidth * 0.13, cHeight * 0.87);
-            break;
-         default:
-            break;
+      for (let i in tiles) {
+         if (tiles[i].label !== "") { tie++; }
       }
+      (tie === 9) ? winner = 2 : tie = 0;
    }
+
+   if (winner === 0 || winner === 1) { score[winner]++; };
 }
 
 var Tile = function(x, y) {
    this.x = x;
-   this.y = y;
-   this.size = cWidth / nCols;
+   this.y = y + 277;
+   this.size = cW / nCols - 7;
    this.label = "";
 };
 
 Tile.prototype.draw = function() {
    fill(color.Black);
    strokeWeight(3);
-   rect(this.x, this.y, this.size - 7, this.size - 7, 11);
+   rect(this.x, this.y, this.size, this.size, 11);
    textSize(79);
    textAlign(CENTER, CENTER);
    fill(color.DarkGoldenRod);
-   text(this.label, this.x + this.size / 2 - 5, this.y + this.size / 2 + 3);
+   text(this.label, this.x + this.size / 2, this.y + this.size / 2 + 2);
 };
 
 Tile.prototype.empty = function() {
@@ -131,11 +121,9 @@ Tile.prototype.onClick = function() {
    if (!this.empty()) { return; }
    // put player's symbol
    this.label = SYMBOLS[pTurn];
-   // change turn
+
+   // 2 players change turn
    (pTurn) ? pTurn = 0 : pTurn = 1;
-   /* // change turn alternative
-   pTurn++;
-   if (pTurn >= SYMBOLS.length) { pTurn = 0; } */
 };
 
 Tile.prototype.handleMouseClick = function(x, y) {
@@ -148,8 +136,16 @@ Tile.prototype.handleMouseClick = function(x, y) {
 
 for (let i = 0; i < nCols; i++) {
    for (let j = 0; j < nRows; j++) {
-      tiles.push(new Tile(i * (cWidth / nCols + 2), j * (cHeight / nRows + 2)));
+      tiles.push(new Tile(i * (cW / nCols + 3), j * (cW / nRows + 3)));
    }
+}
+
+var drawScoreBoard = function() {
+   fill(color.Black);
+   textSize(19);
+   text("Dog: " + score[0], 37, 23);
+   text("Cat: " + score[1], 337, 23);
+   text("Turn: " + SYMBOLS[pTurn], 181, 23);
 }
 
 var drawTiles = function() {
@@ -164,8 +160,72 @@ mouseReleased = function() {
    }
 }
 
+var PCTurn = function() {
+   // VS PC mode
+   do {
+      let r = Math.floor(Math.random() * 9);
+      if (tiles[r].label === "") {
+         tiles[r].label = SYMBOLS[pTurn];
+         pTurn = 0;
+      }
+   } while (pTurn);
+}
+
+var drawWin = function() {
+   stroke(color.FireBrick);
+   strokeWeight(11);
+   
+   switch (winLine) {
+      case 1:
+         line(cW * 0.16, cH * 0.47, cW * 0.16, cH * 0.93);
+         break;
+      case 2:
+         line(cW * 0.5, cH * 0.47, cW * 0.5, cH * 0.93);
+         break;
+      case 3:
+         line(cW * 0.84, cH * 0.47, cW * 0.84, cH * 0.93);
+         break;
+      case 4:
+         line(cW * 0.09, cH * 0.51, cW * 0.91, cH * 0.51);
+         break;
+      case 5:
+         line(cW * 0.09, cH * 0.70, cW * 0.91, cH * 0.70);
+         break;
+      case 6:
+         line(cW * 0.09, cH * 0.89, cW * 0.91, cH * 0.89);
+         break;
+      case 7:
+         line(cW * 0.13, cH * 0.49, cW * 0.87, cH * 0.91);
+         break;
+      case 8:
+         line(cW * 0.87, cH * 0.49, cW * 0.13, cH * 0.91);
+         break;
+      default:
+         break;
+   }
+
+   textSize(61);
+   (winner === 2) ? text("It's a tie", 179, 199) : text(SYMBOLS[winner] + "Wins", 173, 199);
+}
+
+var restartGame = function() {
+   winner = -1;
+   winLine = 0;
+   restartT = 0;
+   tie = 0;
+   for (let i in tiles) { tiles[i].label = ""; }
+   noStroke();
+}
+
 function draw() {
    background(color.DimGrey);
+   drawScoreBoard();
    drawTiles();
-   checkWin();
+   if (winner === -1) {
+      checkWin();
+      // if (pTurn === 1) { PCTurn(); } // Comment for 2 player mode
+   } else {
+      drawWin();
+      if (restartT++ === 139) { restartGame(); }
+   }
 }
